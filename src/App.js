55de5e9041f282deb31a5990e706b95c09e62e00
samia-ctrl <samiauddin99@gmail.com>
2024-01-14 21:37:41 +0000

    default display constructed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-import Weather from "./Weather"
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <Weather />
+      <div>
+        {" "}
+        <a href="">Open-sourced code</a> by <a href="">Samia Ali</a>
+      </div>
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -19,6 +19,7 @@ export default function Search() {
     let apiKey = `535cacbb3f8a0df0aeb4790235b9541f`;
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=metric`;
     axios.get(url).then(showTemp);
+console.log(url)
   }
 
   function updateQuery(event) {
@@ -42,6 +43,9 @@ export default function Search() {
       </div>
     </form>
   );
+
+  let defCity = "London"
+  let defTemperature = 1
   let timeData = {
     date: "Tuesday 19 December",
     time: "10:00",
@@ -61,7 +65,7 @@ export default function Search() {
         <div className="row">
           <div className="col">
             <div className="tempDisplay">
-              {temperature}
+              {Math.round(temperature)}
               <span className="units">
                 <a href="/">°C</a> | <a href="/">°F</a>
               </span>
@@ -104,8 +108,62 @@ export default function Search() {
       </div>
     );
   } else {
-    return <div>{form}
+    return (
+      <div>
+        {form}
+        <h2 className="text-start cityName">{defCity}</h2>
+        <div className="time">
+          <ul>
+            <li>
+              Last updated: {timeData.date} at {timeData.time}
+            </li>
+          </ul>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="tempDisplay">
+              {defTemperature}
+              <span className="units">
+                <a href="/">°C</a> | <a href="/">°F</a>
+              </span>
+            </div>
+          </div>
+          <div className="col">
+            <div>Humidity: %</div>
+            <div>Wind: km/h </div>
+          </div>
+        </div>
 
-    </div>;
+        <div className="forecast">
+          <div className="row">
+            <div className="col-2">
+              Wed
+              <div>img</div>
+              <div>4°C</div>
+            </div>
+            <div className="col-2">
+              Thurs <div>img</div>
+              <div>4°C</div>
+            </div>
+            <div className="col-2">
+              Fri <div>img</div>
+              <div>4°C</div>
+            </div>
+            <div className="col-2">
+              Sat <div>img</div>
+              <div>4°C</div>
+            </div>
+            <div className="col-2">
+              Sun <div>img</div>
+              <div>4°C</div>
+            </div>
+            <div className="col-2">
+              Mon <div>img</div>
+              <div>4°C</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
