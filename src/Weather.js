b79d6525900e4de24f5bba8086166103c4df412f
samia-ctrl <samiauddin99@gmail.com>
2024-02-09 12:07:32 +0000

    h1 name change

--- src/Weather.js
+++ src/Weather.js
@@ -5,7 +5,7 @@ import "./App.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <h2>Weather Application</h2>
+      <h2>React Weather App</h2>
       <Search defaultCity={"London"} />
     </div>
   );
